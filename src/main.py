0243c1d3ed5e72eb2d8884ea54ff1a2bd69c0ec4
Cornelius <sw.cornel@gmail.com>
2026-03-29 22:13:54 +0700

    insert dataset into supabase orders table

--- src/main.py
+++ src/main.py
@@ -1,4 +1,32 @@
+from sqlalchemy import create_engine
 import pandas as pd
-import sqlalchemy
 
-print("Berhasil!")
+# load data
+df = pd.read_csv("data/Superstore.csv", encoding="windows-1252")
+
+# connection
+URL = "postgresql://postgres.kvnklalnsobpxbhsiobv:QKBR6uAV5IFPYqls@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres?sslmode=require"
+
+engine = create_engine(URL)
+
+# insert ke database
+df.to_sql(
+    "orders",  # nama tabel
+    engine,
+    if_exists="replace",  # replace kalau sudah ada
+    index=False,  # jangan simpan index
+    chunksize=1000,  # biar stabil
+)
+
+print("Data berhasil masuk ke tabel 'orders' 🚀")
+
+""" 
+from sqlalchemy import create_engine
+
+DB_URL = "postgresql://postgres.kvnklalnsobpxbhsiobv:QKBR6uAV5IFPYqls@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres?sslmode=require"
+
+engine = create_engine(DB_URL)
+
+with engine.connect() as conn:
+    print("Connected!")
+ """
